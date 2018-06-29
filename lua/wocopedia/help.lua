e64bd26a58d97e72c9a06aa3c05f3b0e943aecb7
gfgtdf <daniel.gfgtdf@gmail.com>
2018-06-30 00:17:49 +0200

    add new help dialog

---
+++ lua/wocopedia/help.lua
@@ -0,0 +1,220 @@
+--<<
+local _ = wesnoth.textdomain 'wesnoth'
+local dialog = wc2_wiki_dialog
+
+function wesnoth.set_dialog_text(text, ...)
+	wesnoth.set_dialog_markup(true, ...)
+	wesnoth.set_dialog_value(text, ...)		
+end
+
+
+local function split_to_array(s)
+	local res = {}
+	for part in tostring(s or ""):gmatch("[^%s,][^,]*") do
+		table.insert(res, part)
+	end
+	return res
+end
+
+local function split_to_set(s)
+	local res = {}
+	for part in tostring(s or ""):gmatch("[^%s,][^,]*") do
+		res[part] = true
+	end
+	return res
+end
+
+function wesnoth.wml_actions.wc2_show_wocopedia(cfg)
+		
+	local show_help_general = true
+	local show_help_training = false
+	local show_help_factions = true
+	local show_help_artifacts = true
+	-- maps the treeview rows to pagenumber in the help page.
+	local index_map = {}
+	local desc_index = 1
+	local ti = {1}
+	local function add_index()
+		index_map[table.concat(ti, "_")] = desc_index
+		desc_index = desc_index + 1
+		ti[#ti] = ti[#ti] + 1
+	end
+	local function tree_enter_mode()
+		ti[#ti] = ti[#ti] - 1
+		table.insert(ti, 1)
+	end
+	local function leave_enter_mode()
+		table.remove(ti)
+		ti[#ti] = ti[#ti] + 1
+	end
+	local current_side = wesnoth.get_viewing_side()
+	local preshow = function()
+		local str_cat_mechnics = _ "Game Mechanics"
+		local str_des_mechnics = 
+			_ "<b>Autorecall</b>:\n" ..
+			_ "Units with trait HEROIC are recalled at start of each scenario with no cost (up to castle size).\n\n" ..
+			_ "<b>Gold</b>:\n" ..
+			_ "Carryover is 15%, comunitary and avoid negative amounts. Early finish bonus is superior to village control, but it is not directly related to their amount.\n\n" ..
+			_ "<b>Trainings</b>:\n" ..
+			_ "Every time you recruit a new unit, your trainings levels will be applied. Every chance will do different dice rolls, if a unit gains training beneficts, you can see them in a trait \"trained\".\n\n" ..
+			_ "<b>Upkeep</b>:\n" ..
+			_ "Units with trait HEROIC or holding any magic ITEM have FREE upkeep.\n\n" ..
+			_ "<b>Army discipline</b>:\n" ..
+			_ "At scenarios 1 to 3, for each training level player already own, trainers found have 2% to 4% chance to become advanced trainers (provide 2 levels). Becomes irrelevant from scenario 4 because all trainers always will be advanced.\n\n" ..
+			_ "<b>Bonus Points</b>:\n" ..
+			_ "In every scenario the game generates as much bonus points oin the map as there are players in the game, the bonus points cn be picked up by players and either contain artifacts, loyal units or training.\n\n" ..
+			""
+		local str_cat_abilities = _ "Abilities"
+		local str_des_abilities = 
+			_ "Ability <b>Autorecall</b>:\n" ..
+			_ "Units with trait HEROIC are recalled at start of each scenario with no cost (up to castle size).\n\n" ..
+			_ "<b>Gold</b>:\n" ..
+			_ "Carryover is 15%, comunitary and avoid negative amounts. Early finish bonus is superior to village control, but it is not directly related to their amount.\n\n" ..
+			_ "<b>Trainings</b>:\n" ..
+			_ "Every time you recruit a new unit, your trainings levels will be applied. Every chance will do different dice rolls, if a unit gains training beneficts, you can see them in a trait \"trained\".\n\n" ..
+			_ "<b>Upkeep</b>:\n" ..
+			_ "Units with trait HEROIC or holding any magic ITEM have FREE upkeep.\n\n" ..
+			_ "<b>Army discipline</b>:\n" ..
+			_ "At scenarios 1 to 3, for each training level player already own, trainers found have 2% to 4% chance to become advanced trainers (provide 2 levels). Becomes irrelevant from scenario 4 because all trainers always will be advanced.\n\n" ..
+			_ "<b>Bonus Points</b>:\n" ..
+			_ "In every scenario the game generates as much bonus points oin the map as there are players in the game, the bonus points cn be picked up by players and either contain artifacts, loyal units or training.\n\n" ..
+			""
+		local str_cat_training = _ "Training"
+		local str_des_training = _ "<b>Training</b>\nTraining impovemes newly recruited units, it has no effect on already recruited units. The follwing list shows all available trainings, the training you currently have is marked in green"
+		local str_cat_items = _ "Artifacts"
+		local str_des_items = _ "<b>Items</b>\nItems can be given to units to make them stronger. You can get artifcats in three ways: 1) By choosing an item as your starting bonus, 2) By finding it on a map ina  bonus point, 3) By dropping from enemies in later scenarios. Note however that not all units can pickup all items."
+		local str_cat_era = _ "Factions"
+		local str_des_era = _ "<b>Factions</b>\n The Word Conquest 2 era consists of faction that are build of pairs of mainline faction of which at one has a healer abailable (Drakes, Rebels and Loyalists), and one does not (Orcs, Dwarves and Undead) the recruilist is also organizes in pairs so that you sometimes have to recruit a different units before you can recruit the units that you want. The available heroes, desertes and random leaders also depend on your factions, the item you can get do not depend on the faction you choose."
+		
+		---- add general topic ----
+		if show_help_general then
+			wesnoth.add_dialog_tree_node("category", ti[1], "left_tree")
+			wesnoth.set_dialog_value(str_cat_mechnics, "left_tree", ti[1], "training_name")
+			wesnoth.set_dialog_value(true, "left_tree", ti[1])
+			wesnoth.add_dialog_tree_node("simpletext", -1, "training_details")
+			wesnoth.set_dialog_text(str_des_mechnics, "training_details", desc_index, "label")
+			add_index()
+		end
+		if show_help_training then
+			---- add general training topic ----
+			wesnoth.add_dialog_tree_node("category", ti[1], "left_tree")
+			wesnoth.set_dialog_value(str_cat_training, "left_tree", ti[1], "training_name")
+			wesnoth.set_dialog_value(true, "left_tree", ti[1])
+			wesnoth.add_dialog_tree_node("simpletext", -1, "training_details")
+			wesnoth.set_dialog_text(str_des_training, "training_details", desc_index, "label")
+			add_index()
+			tree_enter_mode()
+			-- add specific training pages
+			for i = 1, #cdm_training.trainers do
+				local current_level = cdm_training.get_level(current_side, i)
+				local function set_description(train_num, j)
+					local desc = cdm_training.generate_message(i, train_num)
+					if train_num == current_level then
+						desc.caption = "<span color='#00FF00'>" .. desc.caption .. "</span>"
+						desc.message = "<span color='#00FF00'>" .. desc.message .. "</span>"
+					end
+					wesnoth.add_dialog_tree_node("training_details", j, "training_details", desc_index, "tree_details")
+					wesnoth.set_dialog_text(desc.caption, "training_details", desc_index, "tree_details", j, "training_caption")
+					wesnoth.set_dialog_text(desc.message, "training_details", desc_index, "tree_details", j, "training_description")
+				end
+				local trainer = cdm_training.trainers[i]
+				wesnoth.add_dialog_tree_node("training_category", i, "left_tree", ti[1])
+				wesnoth.set_dialog_value(trainer.name, "left_tree", ti[1], ti[2], "training_name")
+				set_description(1, 1)
+				for j = 2, #trainer.grades, 1 do
+					wesnoth.add_dialog_tree_node("seperator", 2*j - 2, "training_details", desc_index, "tree_details")
+					set_description(j, 2*j - 1)
+				end
+				add_index()
+			end
+			leave_enter_mode()
+		end
+		if show_help_factions then
+			local function type_icon(ut)
+				local icon = ut.icon
+				if icon and icon ~= "" then
+					return icon
+				else
+					return ut.image
+				end
+			end
+			---- add general factions topic ----
+			wesnoth.add_dialog_tree_node("category", ti[1], "left_tree")
+			wesnoth.set_dialog_value(str_cat_era, "left_tree", ti[1], "training_name")
+			wesnoth.set_dialog_value(true, "left_tree", ti[1])
+			wesnoth.add_dialog_tree_node("simpletext", -1, "training_details")
+			wesnoth.set_dialog_text(str_des_era, "training_details", desc_index, "label")
+			add_index()
+			tree_enter_mode()
+			for i, faction_info in ipairs(wc2_era.factions_wml) do
+				wesnoth.add_dialog_tree_node("training_category", ti[2], "left_tree", ti[1])
+				wesnoth.set_dialog_value(faction_info.name, "left_tree", ti[1], ti[2], "training_name")
+				wesnoth.add_dialog_tree_node("faction_info", -1, "training_details")
+				j = 0
+				for p_wml in wml.child_range(faction_info, "pair") do
+					j = j + 1
+					local p = split_to_array(p_wml.types)
+					local ut1 = wesnoth.unit_types[p[1]] or error("invald unit type" .. tostring(p[1]))
+					local ut2 = wesnoth.unit_types[p[2]] or error("invald unit type" .. tostring(p[2]))
+				
+					wesnoth.add_dialog_tree_node("recruit_pair", j, "training_details", desc_index, "recruit_pairs")
+					wesnoth.set_dialog_text(ut1.name, "training_details", desc_index, "recruit_pairs", j, "label1")
+					print(tostring(ut1.icon or ut1.image))
+					wesnoth.set_dialog_value(type_icon(ut1), "training_details", desc_index, "recruit_pairs", j, "image1")
+					wesnoth.set_dialog_text(ut2.name, "training_details", desc_index, "recruit_pairs", j, "label2")
+					wesnoth.set_dialog_value(type_icon(ut2), "training_details", desc_index, "recruit_pairs", j, "image2")
+				end
+				wesnoth.set_dialog_text(faction_info.deserters_names, "training_details", desc_index, "deserters")
+				
+				add_index()
+			end
+			leave_enter_mode()
+		end
+		---- add general bonus point topic ----
+		--wesnoth.add_dialog_tree_node("category", ti[1], "left_tree")
+		--wesnoth.set_dialog_value(str_cat_bonus, "left_tree", ti[1], "training_name")
+		--wesnoth.add_dialog_tree_node("simpletext", -1, "training_details")
+		--wesnoth.set_dialog_text(str_des_bonus, "training_details", desc_index, "label")
+		--add_index()
+		
+		if show_help_artifacts then
+			local str_not_for_enemies = _ " (not available for enemies)"
+			local str_not_for_players = _ " (not available for players)"
+
+			wesnoth.add_dialog_tree_node("category", ti[1], "left_tree")
+			wesnoth.set_dialog_value(str_cat_items, "left_tree", ti[1], "training_name")
+			wesnoth.add_dialog_tree_node("artifact_list", -1, "training_details")
+			wesnoth.set_dialog_text(str_des_items, "training_details", desc_index, "desc")
+		
+			for i = 0, (wml.variables["wct.artifact.length"] - 1) do
+				local artifact_icon = wml.variables["wct.artifact[" .. i .. "].icon"] or ""
+				local artifact_name = wml.variables["wct.artifact[" .. i .. "].name"] or ""
+				local artifact_desc = wml.variables["wct.artifact[" .. i .. "].description"] or ""
+				local not_available = split_to_set(wml.variables["wct.artifact[" .. i .. "].not_available"] or "")
+
+				if not_available.player then
+					artifact_name = artifact_name .. str_not_for_players
+				end
+				if not_available.enemy then
+					artifact_name = artifact_name .. str_not_for_enemies
+				end
+				wesnoth.add_dialog_tree_node("artifact", i + 1, "training_details", desc_index, "artifact_list_tv")
+				wesnoth.set_dialog_value(artifact_icon, "training_details", desc_index, "artifact_list_tv", i + 1, "image")
+				wesnoth.set_dialog_value(artifact_name .. "\n" .. artifact_desc, "training_details", desc_index, "artifact_list_tv", i + 1, "label")
+			end
+			add_index()
+		end
+		wesnoth.set_dialog_focus("left_tree")
+
+		wesnoth.set_dialog_callback(function()
+			local selected = wesnoth.get_dialog_value("left_tree")
+			local selected_page_index = index_map[table.concat(selected, '_')]
+			if selected_page_index ~= nil then
+				wesnoth.set_dialog_value(selected_page_index, "training_details")
+			end
+		end, "left_tree")
+	end
+	
+	wesnoth.show_dialog(dialog, preshow)
+end
+-->>
---
+++ lua/wocopedia/help_dialog.lua
@@ -0,0 +1,488 @@
+--<<
+local _ = wesnoth.textdomain 'wesnoth'
+
+local function GUI_FORCE_WIDGET_MINIMUM_SIZE(w,h, content)
+	return T.stacked_widget {
+		definition = "default",
+		T.stack {
+			T.layer {
+				T.row {
+					T.column {
+						T.spacer {
+							definition = "default",
+							width = w,
+							height = h
+						}
+					}
+				} 
+			},
+			T.layer {
+				T.row {
+					grow_factor = 1,
+					T.column {
+						grow_factor = 1,
+						horizontal_grow = "true",
+						vertical_grow = "true",
+						content
+					}
+				} 
+			}
+		}
+	}
+end
+
+
+return {
+	maximum_width = 900,
+	T.helptip { id = "tooltip_large" }, -- mandatory field
+	T.tooltip { id = "tooltip_large" }, -- mandatory field
+		
+	T.linked_group { id = "artifact_icon", fixed_width = true },
+	T.linked_group { id = "artifact_name", fixed_width = true },
+	T.linked_group { id = "recruit_icon", fixed_width = true },
+	T.linked_group { id = "recruit_name", fixed_width = true },
+
+	T.grid {
+		T.row {
+			grow_factor = 1,
+			T.column {
+				border = "all",
+				border_size = 5,
+				horizontal_alignment = "left",
+				T.label {
+					definition = "title",
+					label = _"WoCopedia",
+					id = "title"
+				}
+			}
+		},
+		T.row {
+			grow_factor = 1,
+			T.column {
+				horizontal_grow = true,
+				vertical_grow = true,
+				T.grid {
+					T.row {
+						T.column {
+							border = "all",
+							border_size = 5,
+							horizontal_grow = true,
+							vertical_grow = true,
+							T.tree_view {
+								id = "left_tree",
+								definition = "default",
+								horizontal_scrollbar_mode = "never",
+								vertical_scrollbar_mode = "never",
+								indentation_step_size = 35,
+								T.node {
+									id = "training_category",
+									T.node_definition {
+										T.row {
+											T.column {
+												grow_factor = 1,
+												horizontal_grow = true,
+												T.toggle_panel {
+													id = "tree_view_node_label",
+													T.grid {
+														T.row {
+															T.column {
+																horizontal_alignment = "left",
+																T.label {
+																	horizontal_alignment = "left",
+																	id = "training_name",
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+								T.node {
+									id = "category",
+									T.node_definition {
+										T.row {
+											T.column {
+												grow_factor = 0,
+												horizontal_grow = true,
+												T.toggle_button {
+													id = "tree_view_node_toggle",
+													definition = "tree_view_node",
+												},
+											},
+											T.column {
+												grow_factor = 1,
+												horizontal_grow = true,
+												T.toggle_panel {
+													id = "tree_view_node_label",
+													T.grid {
+														T.row {
+															T.column {
+																horizontal_alignment = "left",
+																T.label {
+																	horizontal_alignment = "left",
+																	id = "training_name",
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						T.column {
+							T.multi_page {
+								id = "training_details",
+								definition = "default",
+								horizontal_scrollbar_mode = "never",
+								T.page_definition {
+									T.row {
+										T.column {
+											vertical_alignment = "top",
+											horizontal_alignment = "left",
+											horizontal_grow = true,
+											vertical_grow = true,
+											T.scrollbar_panel { 
+												T.definition { 
+													T.row { 
+														T.column {
+															vertical_alignment = "top",
+															horizontal_alignment = "left",
+															horizontal_grow = true,
+															vertical_grow = true,
+															T.tree_view {
+																id = "tree_details",
+																definition = "default",
+																horizontal_scrollbar_mode = "never",
+																vertical_scrollbar_mode = "never",
+																indention_step_size = 20,
+																T.node {
+																	id = "training_details",
+																	T.node_definition {
+																		T.row {
+																			T.column {
+																				grow_factor = 1,
+																				horizontal_alignment = "left",
+																				T.label {
+																					definition = "default_large",
+																					horizontal_alignment = "left",
+																					id = "training_caption",
+																					label = "default description",
+																				},
+																			},
+																		},
+																		T.row {
+																			T.column {
+																				grow_factor = 1,
+																				horizontal_alignment = "left",
+																				T.label {
+																					horizontal_alignment = "left",
+																					id = "training_description",
+																					label = "default description",
+																				},
+																			},
+																		},
+																	},
+																},
+																T.node {
+																	id = "scroll_details",
+																	T.node_definition {
+																		T.row {
+																			T.column {
+																				grow_factor = 1,
+																				horizontal_alignment = "left",
+																				T.label {
+																					definition = "default_large",
+																					horizontal_alignment = "left",
+																					id = "training_caption",
+																					label = "default description",
+																				},
+																			},
+																		},
+																		T.row {
+																			T.column {
+																				grow_factor = 1,
+																				horizontal_alignment = "left",
+																				T.scroll_label {
+																					horizontal_alignment = "left",
+																					id = "training_description",
+																					label = "default description",
+																				},
+																			},
+																		},
+																	},
+																},
+																T.node {
+																	id = "seperator",
+																	T.node_definition {
+																		T.row {
+																			T.column {
+																				grow_factor = 1,
+																				horizontal_grow = true,
+																				T.spacer {
+																					height = 20,
+																					width = 20,
+																				},
+																			},
+																		},
+																	},
+																},
+															} 
+														} 
+													}
+												} 
+											} 
+										}
+									}
+								},
+								T.page_definition {
+									id="simpletext",
+									T.row {
+										T.column {
+											horizontal_grow = true,
+											vertical_grow = true,
+											T.scroll_label {
+												id = "label",
+												label = "Text",
+											} 
+										}
+									}
+								},
+								T.page_definition {
+									id="artifact_list",
+									vertical_grow = true,
+									T.row {
+										T.column {
+											vertical_grow = true,
+											T.grid {
+												--T.row {
+												--	T.column {
+												--		grow_factor = 1,
+												--		vertical_grow = true,
+												--		T.label {
+												--			id = "title",
+												--			label = "Artifacts",
+												--		}
+												--	}
+												--},
+												T.row {
+													T.column {
+														T.scroll_label {
+															vertical_scrollbar_mode = "never",
+															id = "desc",
+														}
+													}
+												},
+												T.row {
+													T.column {
+														horizontal_grow = true,
+														T.tree_view {
+															vertical_grow = true,
+															id = "artifact_list_tv",
+															definition = "default",
+															horizontal_scrollbar_mode = "never",
+															vertical_scrollbar_mode = "always",
+															indentation_step_size = 35,
+															T.node {
+																id = "artifact",
+																horizontal_grow = true,
+																T.node_definition {
+																	vertical_grow = true,
+																	T.row {
+																		horizontal_grow = true,
+																		T.column {
+																			horizontal_grow = true,
+																			T.grid {
+																				T.row {
+																					T.column {
+																						T.image {
+																							id="image",
+																							linked_group = "artifact_icon",
+																						}
+																					},
+																					T.column {
+																						T.label {
+																							id="label",
+																							linked_group = "artifact_name",
+																						}
+																					},
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												},
+												T.row {
+													grow_factor = 1,
+													T.column {
+														grow_factor = 0,
+														vertical_grow = true,
+														T.spacer {
+															id = "aa",
+														}
+													}
+												},
+											}
+										}
+									}
+								},
+								T.page_definition {
+									id="faction_info",
+									vertical_grow = true,
+									T.row {
+										T.column {
+											vertical_grow = true,
+											horizontal_grow = true,
+											T.grid {
+												--T.row {
+												--	T.column {
+												--		grow_factor = 1,
+												--		vertical_grow = true,
+												--		T.label {
+												--			id = "title",
+												--			label = "Artifacts",
+												--		}
+												--	}
+												--},
+												T.row {
+													T.column {
+														T.scroll_label {
+															vertical_scrollbar_mode = "never",
+															id = "desc",
+														}
+													}
+												},
+												T.row {
+													T.column {
+														T.label {
+															vertical_scrollbar_mode = "never",
+															label = "<b>Recruits:</b>",
+															use_markup = true,
+														}
+													}
+												},
+												T.row {
+													T.column {
+														horizontal_grow = true,
+														T.tree_view {
+															id = "recruit_pairs",
+															definition = "default",
+															horizontal_scrollbar_mode = "never",
+															vertical_scrollbar_mode = "always",
+															indentation_step_size = 35,
+															T.node {
+																id = "recruit_pair",
+																horizontal_grow = true,
+																T.node_definition {
+																	T.row {
+																		T.column {
+																			horizontal_grow = true,
+																			T.grid {
+																				T.row {
+																					T.column {
+																						T.image {
+																							id="image1",
+																							linked_group = "recruit_icon",
+																						}
+																					},
+																					T.column {
+																						T.label {
+																							id="label1",
+																							linked_group = "recruit_name",
+																						}
+																					},
+																					T.column {
+																						T.label {
+																							label="↔",
+																						}
+																					},
+																					T.column {
+																						T.image {
+																							id="image2",
+																							linked_group = "recruit_icon",
+																						}
+																					},
+																					T.column {
+																						T.label {
+																							id="label2",
+																							linked_group = "recruit_name",
+																						}
+																					},
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												},
+												T.row {
+													T.column {
+														T.label {
+															vertical_scrollbar_mode = "never",
+															label = "<b>Deserters:</b>",
+															use_markup = true,
+														}
+													}
+												},
+												T.row {
+													T.column {
+														T.scroll_label {
+															vertical_scrollbar_mode = "never",
+															id = "deserters",
+															use_markup = true,
+														}
+													}
+												},
+												T.row {
+													grow_factor = 1,
+													T.column {
+														grow_factor = 0,
+														vertical_grow = true,
+														T.spacer {
+															id = "aa",
+														}
+													}
+												},
+											}
+										}
+									}
+								}
+							},
+						}
+					}
+				}
+			}
+		},
+		T.row {
+			T.column {
+				horizontal_grow = true,
+				T.grid {
+					T.row {
+						T.column {
+							grow_factor = 1,
+							T.spacer {
+							}
+						},
+						T.column {
+							horizontal_alignment = "right",
+							T.button {
+								label = "Ok",
+								id = "ok",
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+-->>
