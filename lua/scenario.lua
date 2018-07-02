bf2ec276f856214b83965b70dd18b4393eeaa055
gfgtdf <daniel.gfgtdf@gmail.com>
2018-07-02 19:14:29 +0200

    add version getter

--- lua/scenario.lua
+++ lua/scenario.lua
@@ -1,7 +1,17 @@
 local wc2_scenario = {}
+local on_event = wesnoth.require("on_event")
 
 function wc2_scenario.is_human_side(side_num)
 	return side_num == 1 or side_num == 2 or side_num == 3
 end
+
+on_event("preload", function()	
+	wml.variables["wc2.original_version"] = "0.7.0"
+	wml.variables["wc2.version"] = "0.7.0"
+end)
+
+on_event("scenario_end", function()
+	wml.variables["wc2.version"] = nil
+end)
 
 return wc2_scenario
