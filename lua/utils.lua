8ea798fe4f13323eaed3f7b3519f5bc962743965
gfgtdf <daniel.gfgtdf@gmail.com>
2018-07-03 13:06:06 +0200

    new utils functions

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,5 +1,7 @@
 --<<
 local wc2_utils = {}
+-- variabel access with 
+--local wc2_utils.vars = {}
 
 function wc2_utils.split_to_array(s, res)
 	res = res or {}
@@ -26,6 +28,30 @@ function wc2_utils.remove_dublicates(t)
 		else
 			found[v] = true
 		end
+	end
+end
+
+function wc2_utils.wml_sub(str)
+	return wml.tovconfig({str = str}).str
+end
+
+function wc2_utils.pick_random(str)
+	local size = wml.variables[str .. ".length"]
+	if size ~= 0 then
+		local index = wesnoth.random(size) - 1
+		local res = wml.variables[str .. "[" ..  index .. "]"]
+		wml.variables[str .. "[" ..  index .. "]"] = nil
+		return res
+	end
+	-- maybe it not a wml arra  but a comma seperated list
+	local s2 = wml.variables[str]
+	if s2 ~= nil then
+		local array = wc2_utils.split_to_array(s2)
+		local index  = wesnoth.random(#array)
+		local res = array[index]
+		table.remove(array, index)
+		wml.variables[str] = table.concat(array, ",")
+		return res
 	end
 end
 
