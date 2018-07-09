b35d61b261832d641e6f71c9acb6b7f2bfa1489f
gfgtdf <daniel.gfgtdf@gmail.com>
2018-07-09 22:47:48 +0200

    fix experience_penalty for leader

--- lua/scenario.lua
+++ lua/scenario.lua
@@ -31,6 +31,21 @@ on_event("recruit", 1, function(ec)
 	u:add_modification("advancement", { wc2_scenario.experience_penalty() })
 end)
 
+function wesnoth.wml_actions.wc2_start_units(cfg)
+	local u = wesnoth.get_units({ side = cfg.side, canrecruit = true })[1]
+	u:add_modification("advancement", { wc2_scenario.experience_penalty() })
+	u:add_modification("trait", wc2_heroes.trait_heroic )
+	u.hitpoints = u.max_hitpoints
+	u.moves = u.max_moves
+	for i = 1, wml.variables["difficulty.heroes"] do
+		wesnoth.wml_actions.wc2_random_hero {
+			x = u.x,
+			y = u.y,
+			side = u.side,
+		}
+	end
+end
+
 on_event("scenario_end", function()
 	wml.variables["wc2.version"] = nil
 end)
