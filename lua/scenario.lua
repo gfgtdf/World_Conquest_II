e43c8f6642b862b163386e3fa32419bd98176e6d
gfgtdf <daniel.gfgtdf@gmail.com>
2019-08-16 18:54:34 +0200

    v 0.8 fixup

--- lua/scenario.lua
+++ lua/scenario.lua
@@ -19,9 +19,9 @@ end
 
 on_event("preload", function()
 	if not wml.variables["wc2.original_version"] then
-		wml.variables["wc2.original_version"] = "0.7.10.2"
+		wml.variables["wc2.original_version"] = "0.8.0"
 	end
-	wml.variables["wc2.version"] = "0.7.10.2"
+	wml.variables["wc2.version"] = "0.8.0"
 end)
 
 -- happens before training events.
