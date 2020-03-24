66d51d0e9a49d82d17d3a59dc7229d32422bc30b
gfgtdf <daniel.gfgtdf@gmail.com>
2020-03-24 22:57:03 +0100

    move era.lua

--- lua/campaign_main.lua
+++ lua/campaign_main.lua
@@ -1,10 +1,12 @@
+-- the main file for the WC2 mp campaign
+
 T = wml.tag
 on_event = wesnoth.require("on_event")
 
 
 wesnoth.dofile("./game_mechanics/_load.lua")
 
-wc2_era = wesnoth.require("./era.lua")
+wc2_era = wesnoth.require("./era/era.lua")
 
 wc2_enemy = wesnoth.dofile("./campaign/enemy.lua")
 
