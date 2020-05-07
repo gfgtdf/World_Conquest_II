5861b9d1042fd10a8f7f5c42138f2a403a36ee6e
gfgtdf <daniel.gfgtdf@gmail.com>
2020-05-08 01:47:29 +0200

    remove 1.15.3 compatabiliy code
    this is broken on 1.15.3 anyways, due to the treeview lua callback bug

--- lua/game_mechanics/color.lua
+++ lua/game_mechanics/color.lua
@@ -1,46 +1,8 @@
 --<<
 local color = {}
--- TODO: remove after 1.15.4
-local color_info = {
-	red    = { r = 178, g =  38 , b =  38 },
-	blue   = { r =  88, g = 103 , b = 175 },
-	green  = { r =  78, g = 146 , b =  80 },
-	purple = { r = 118, g =  00 , b = 126 },
-	black  = { r =  72, g =  72 , b =  72 },
-	brown  = { r = 118, g =  64 , b =  31 },
-	orange = { r = 204, g = 101 , b =  00 },
-	white =  { r = 205, g = 205 , b = 205 },
-	teal  =  { r =  38, g = 162 , b = 154 },
-	["1"] =  { r = 178, g =  38 , b =  38 },
-	["2"] =  { r =  88, g = 103 , b = 175 },
-	["3"] =  { r =  78, g = 146 , b =  80 },
-	["4"] =  { r = 118, g =  00 , b = 126 },
-	["5"] =  { r =  72, g =  72 , b =  72 },
-	["6"] =  { r = 118, g =  64 , b =  31 },
-	["7"] =  { r = 204, g = 101 , b =  00 },
-	["8"] =  { r = 205, g = 205 , b = 205 },
-	["9"] =  { r =  38, g = 162 , b = 154 },
-}
 
 function color.to_pango_string(c)
 	return ("#%02x%02x%02x"):format(c.r, c.g, c.b)
-end
-
-function color.get_color_info(id)
-	local res = color_info[id]
-	if not res then
-		print ("unknonw color id:" .. color)
-		res = { r = 205, g = 205 , b = 205 }
-	end
-	return res
-end
-
-function wesnoth.wml_actions.wc2_convert_color(cfg)
-	local color_id = tostring(cfg.color)
-	local res = color.get_color_info(color_id)
-	local var = cfg.variable
-	wml.variables[var .. ".text"] = res.text
-	wml.variables[var .. ".rgb"] = res.rgb
 end
 
 function color.color_text(color_str, text)
@@ -54,14 +16,8 @@ function color.tc_text(team_num, text)
 		text = team_num
 		team_num = wesnoth.current.side
 	end
-	local c = nil
+	local c = wesnoth.colors[wesnoth.sides[team_num].color].mid
 
-	if wesnoth.colors then
-		c = wesnoth.colors[wesnoth.sides[team_num].color].mid
-	else
-		c = color.get_color_info(wesnoth.sides[team_num].color)
-	end
-	
 	local color_str = color.to_pango_string(c)
 	return color.color_text(color_str, text)
 end
